f816c632ad0925f7e2c10b0c829796f72fefb4aa
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-11 14:42:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -285,7 +285,7 @@ if so_file:
         # Merge results into a single DataFrame
         final_results_df = results[0]
         for df in results[1:]:
-            final_results_df = final_results_df.merge(df, on=["WH ID", "Hub ID","product_id", "Sum of maxqty"], how="left")
+            final_results_df = final_results_df.merge(df, on=["WH ID", "Hub ID","product_id", "Sum of maxqty","stock"], how="left")
         final_results_df2 = final_results_df.drop_duplicates()
         #final_results_df["WH Name"] = final_results_df["wh_id"].map(wh_name_mapping)
         
