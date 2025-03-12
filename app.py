0f6d0696794a00a1b64f2601f1a4f454afeb6bd7
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-12 13:49:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -256,8 +256,8 @@ if so_file:
                     #total_maxqty = final_so_df.loc[final_so_df['WH ID'] == wh_id, 'Sum of maxqty'].sum()
                     
                     #if total_so_final > 0:
-                        hub_forecast = ((final_so_df.loc[hub_mask, 'Sum of maxqty'] / total_maxqty) * 
-                                        (dry_demand_allocation_split.get(wh_id, 0)))
+                        #hub_forecast = ((final_so_df.loc[hub_mask, 'Sum of maxqty'] / total_maxqty) * 
+                                        #(dry_demand_allocation_split.get(wh_id, 0)))
                     #else:
                         #hub_forecast = 0
                     
